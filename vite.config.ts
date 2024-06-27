8575d00131511fdc35715e80c464c0dc812b6a78
Samira Rezayi <sre@adlas.at>
2024-06-27 10:57:18 +0330

    fix(todo): change the search todo

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(),vuetify(), viteTsconfigPaths()],
+    server: {
+        port: 5000,
+    },
 })
